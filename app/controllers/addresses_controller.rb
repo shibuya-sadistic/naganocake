d702751695f479632a5b96184d5d1712bc327dd6
gatescchi <t.ozawa0723@gmail.com>
2020-04-15 11:32:43 +0900

    [add]controllers

---
+++ app/controllers/addresses_controller.rb
@@ -0,0 +1,17 @@
+class AddressesController < ApplicationController
+end
+
+def index
+end
+
+def create
+end
+
+def destroy
+end
+
+def edit
+end
+
+def update
+end
---
+++ app/controllers/cart_items_controller.rb
@@ -0,0 +1,12 @@
+class CartItemsController < ApplicationController
+end
+def create
+end
+def index
+end
+def update
+end
+def destroy
+end
+def destroy_all
+end
