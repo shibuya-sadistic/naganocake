675220f9dcab566831014c2f3a95c8cc2906a86a
sekine11 <sekine11665@gmail.com>
2020-04-18 01:02:28 +0900

    [Add] view and action

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,12 @@
 class ProductsController < ApplicationController
   def index
+  	@products = Product.all
+  	@genres = Genre.all
   end
 
   def show
+  	@product = Product.find(params[:id])
+  	@genres = Genre.all
+  	@cart_item = CartItem.new
   end
 end
