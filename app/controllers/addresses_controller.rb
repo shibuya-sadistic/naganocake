03bc1a87b9ca5bfa79e9347297c90b57276a7ab4
hikari24 <a.hikari24@gmail.com>
2020-04-20 01:16:47 +0000

    [Add]address_controller

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -1,18 +1,47 @@
 class AddressesController < ApplicationController
 
 def index
+	@addresses = Address.all
+	@address = Address.new
 end
 
 def create
-end
-
-def destroy
+	@address = Address.new(address_params)
+	@address.customer_id = current_customer.id
+	if @address.save
+	redirect_to addresses_path(@address)
+	else
+	@addresses = Address.all
+	@address = Address.new
+	render 'index'
+	end
 end
 
 def edit
+	@address = Address.find(params[:id])
 end
 
 def update
+	@address = Address.find(params[:id])
+	if @address.update(address_params)
+       redirect_to addresses_path(@address)
+    else
+       @address = Address.find(params[:id])
+       render 'edit'
+    end
 end
 
+def destroy
+	@address = Address.find(params[:id])
+	@address.destroy
+	redirect_to addresses_path(@address)
+end
+
+
+
+private
+	def address_params
+		params.require(:address).permit(:customer_id, :address, :postcode, :destination)
+ 
+	end
 end
