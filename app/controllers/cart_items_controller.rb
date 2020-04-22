590a342df782a12a7b67470d987bb4f0fac82c58
gatescchi <t.ozawa0723@gmail.com>
2020-04-22 12:17:55 +0900

    [Add]view,controller

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -1,26 +1,36 @@
 class CartItemsController < ApplicationController
-	before_action :authenticate_user!
+before_action :authenticate_customer!
 def create
-	@cart_item=Cart_item.new(cart_item.id)
-	@cart_item.save
-	redirect_to_root_path
+
+	@cart_item = CartItem.new(cart_item_params)
+	@cart_item.customer_id = current_customer.id
+	if @cart_item.save
+		redirect_to root_path, notice:"カートに商品が追加されました。"
+    else render 'index'
+    end
 end
 def index
 	@cart_items = current_customer.cart_items.all
-	@product=@cart_item.product
 end
 def update
-	@cart_item = Cart_item.find(params[:id])
+	@cart_item = CartItem.find(params[:id])
+	@cart_item.update(cart_item_params)
+	redirect_to cart_items_path
 end
+
 def destroy
-	cart_item=Cart_item.find(palams[:id])
+	cart_item=CartItem.find(params[:id])
+	cart_item.destroy
+	redirect_to cart_items_path
 end
 def destroy_all
-	cart_item=Cart_item.find(palams[:id])
+	cart_items = current_customer.cart_items
+	cart_items.destroy_all
+	redirect_to cart_items_path,notice:"カートを空にしました"
 end
 
 private
 def cart_item_params
-	params.require(:cart_item).parmit(:piece, :customer_id, :product_id)
+	params.require(:cart_item).permit(:piece, :product_id)
 end
 end
