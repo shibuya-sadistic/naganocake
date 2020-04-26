a5d0fd8a99be147394edee06257e6c32d8f4661c
hikari24 <a.hikari24@gmail.com>
2020-04-26 09:45:15 +0000

    [Add]test

--- app/controllers/admins/home_controller.rb
+++ app/controllers/admins/home_controller.rb
@@ -1,4 +1,5 @@
 class Admins::HomeController < ApplicationController
+  before_action :authenticate_admin!
   def top
   	@orders = Order.where(created_at: Time.zone.today.beginning_of_day..Time.zone.today.end_of_day)
   end
